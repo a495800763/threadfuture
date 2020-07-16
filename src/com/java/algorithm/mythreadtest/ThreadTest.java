d1c03f342f2acf69b585ac1d0e6872970ef5c495
a495800763 <495800763@qq.com>
2020-07-16 22:23:43 +0800

    thread new

---
+++ src/com/java/algorithm/mythreadtest/ThreadTest.java
@@ -0,0 +1,9 @@
+package com.java.algorithm.mythreadtest;
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        Thread.State state = t.getState();
+        System.out.println(state);
+    }
+}
